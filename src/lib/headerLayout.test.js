d51c36f650a68600836907af179c034ef1c39cc0
Dylan Watson <lotsabackscatter@gmail.com>
2026-07-22 13:12:04 +0800

    Match route controls to search button size

--- src/lib/headerLayout.test.js
+++ src/lib/headerLayout.test.js
@@ -86,8 +86,9 @@ test('route and pin controls are icon buttons below the map zoom controls', () =
     assert.match(css, /\.route-planner\s*{[^}]*top:\s*calc\(var\(--header-offset\)\s*\+\s*92px\)/s);
     assert.match(css, /\.route-planner\s*{[^}]*right:\s*var\(--map-control-right\)/s);
     assert.doesNotMatch(css, /\.route-planner\s*{[^}]*left:\s*14px/s);
-    assert.match(css, /\.route-planner-actions button\s*{[^}]*width:\s*42px/s);
-    assert.match(css, /\.route-planner-actions button\s*{[^}]*height:\s*42px/s);
+    assert.match(css, /\.route-planner-actions\s*{[^}]*padding:\s*3px/s);
+    assert.match(css, /\.route-planner-actions button\s*{[^}]*width:\s*34px/s);
+    assert.match(css, /\.route-planner-actions button\s*{[^}]*height:\s*34px/s);
     assert.match(css, /\.route-planner-actions\s*{[^}]*display:\s*grid/s);
     assert.match(css, /\.route-planner-card\s*{[^}]*width:\s*max-content/s);
     assert.match(css, /\.route-planner-card\s*{[^}]*max-width:\s*min\(190px,\s*calc\(100vw - 110px\)\)/s);
